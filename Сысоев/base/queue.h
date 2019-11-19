b136819d0745504dc5c255eefc2d82888e1b83a7
BoytsovVA <vladboi8@gmail.com>
2019-11-19 03:37:10 +0300

    @BoytsovVA Version 1

---
+++ Сысоев/base/queue.h
@@ -0,0 +1,119 @@
+﻿#ifndef __STACK_H__
+#define __STACK_H__
+using namespace std;
+const int MaxQueueSize = 100;
+
+template <class T>
+class TQueue
+{
+private:
+	T* pMem;
+	int size; //Размер очереди 
+	int last;
+	int first;
+	int count;
+public:
+	TQueue(int _size)
+	{
+		size = _size;
+		first = -1;
+		last = 0;
+		count = 0;
+		if ((size < 0) || (size > MaxQueueSize))
+			throw size;
+		pMem = new T[size];
+	}
+
+	TQueue(TQueue& v)
+	{
+		size = v.size;
+		pMem = new T[size];
+		count = v.count;
+		first = v.first;
+		last = v.last;
+		for (int i = 0; i < size; i++)
+			pMem[i] = v.pMem[i];
+	}
+
+	~TQueue()
+	{ 
+		delete [] pMem; 
+	}
+
+	bool IsEmpty() { return count == 0; }
+	bool IsFull() { return count == size; }
+	int GetSize() { return size; }  //Геттер
+	int GetCount() { return count; }
+	
+	T Pop() //Взять элемент
+	{
+		if (!IsEmpty())
+		{
+			count--;
+			first = (first+ 1) % size;
+			return pMem[first];
+		}
+		else
+			throw "Error";
+	}
+
+
+	void Push(T v) //Положить элемент
+	{
+		if (!IsFull())
+		{
+			count++;
+			pMem[last] = v;
+			last = (last + 1) % size;
+		}
+		else
+			throw "Error";
+	}
+
+	T GetFirst() { return pMem[(first + 1) % size]; }
+	T GetLast() { return pMem[last]; }
+
+	TQueue& operator=(const TQueue& v)
+	{
+		if (size != v.size)
+		{
+			delete[] pMem;
+			size = v.size;
+			first = v.first;
+			last = v.last;
+			count = v.count;
+			pMem = new T[size];
+			for (int i = 0; i < size; i++)
+				pMem[i] = v.pMem[i];
+		}
+		else
+		{
+			for (int i = 0; i < size; i++)
+				pMem[i] = v.pMem[i];
+			count = v.count;
+			first = v.first;
+			last = v.last;
+		}
+		return *this;
+	}
+
+	bool operator==(const TQueue& v) const
+	{
+		if (this->size != v.size)
+			return false;
+		else
+		{
+			for (int i = 0; i < size; i++)
+			{
+				if (this->pMem[i] != v.pMem[i])
+					return false;
+			}
+			return true;
+		}
+	}
+	bool operator!=(const TQueue& v) const { return!(*this == v); }
+
+	
+};
+
+#endif
---
+++ Сысоев/base/sample_classter.cpp
@@ -0,0 +1,35 @@
+﻿#include <iostream>
+#include <string>
+#include "classter.h"
+
+
+using namespace std;
+
+int main()
+{
+	setlocale(LC_ALL, "Russian");
+	int tacts, sizequeue, proc;
+	double intens, power;
+	cout << "Введите параметры класстера" << endl;
+
+	cout << "Задайте время работы системы (В тактах)" << endl;
+	cin >> tacts;
+
+	cout << "Задайте размер очереди" << endl;
+	cin >> sizequeue;
+
+	cout << "Задайте мощность класстера" << endl;
+	cin >> power;
+
+	cout << "Задайте интенсивность потока задач" << endl;
+	cin >> intens;
+
+	cout << "Задайте количество процессоров в кластере" << endl;
+	cin >> proc;
+
+	TClasster classter(tacts, sizequeue, intens, power, proc);
+	classter.Run();
+	classter.Get_Status();
+
+  return 0;
+}
